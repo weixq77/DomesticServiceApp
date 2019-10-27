708cd23d1040ad5a57f09f6f8c0e82e4d0dff036
= <=>
2019-10-27 21:13:49 +0800

    [wxq]完善订单页面

--- src/pages/manager/Order.vue
+++ src/pages/manager/Order.vue
@@ -2,7 +2,18 @@
     <div id="order">
         <van-nav-bar title="订单" />
         <van-tabs v-model="activeName" swipeable>
-            <van-tab title="全部订单" name="a">全部订单</van-tab>
+            <van-tab title="全部订单" name="a">
+                <van-card
+                    num="2"
+                    price="2.00"
+                    desc="描述信息"  
+                    title="商品标题"
+                    thumb="https://img.yzcdn.cn/vant/t-thirt.jpg">
+                    <div slot="bottom">
+                        {{未服务}}
+                    </div>
+                </van-card>
+            </van-tab>
             <van-tab title="待付款" name="b">待付款</van-tab>
             <van-tab title="待服务" name="c">待服务</van-tab>
             <van-tab title="待评价" name="d">待评价</van-tab>
