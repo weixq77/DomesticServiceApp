11565d8e602eea7c1a4404a013ccf2da4b02f5db
= <=>
2019-10-28 00:25:05 +0800

    [wxq]完善订单页面代码

--- src/pages/manager/Order.vue
+++ src/pages/manager/Order.vue
@@ -1,35 +1,169 @@
 <template>
     <div id="order">
-        <van-nav-bar title="订单" />
-        <van-tabs v-model="activeName" swipeable>
+        
+        <div class="orderTab">
+            <van-nav-bar title="订单" fixed/>
+        </div>
+            
+        <van-tabs v-model="activeName" swipeable :offset-top="46" sticky>
             <van-tab title="全部订单" name="a">
-                <van-card
-                    num="2"
-                    price="2.00"
-                    desc="描述信息"  
-                    title="商品标题"
-                    thumb="https://img.yzcdn.cn/vant/t-thirt.jpg">
-                    <div slot="bottom">
-                        {{未服务}}
-                    </div>
-                </van-card>
+                <div v-if="orders!=''">
+                    <van-card
+                        v-for=" item in orders"
+                        :key="item.id"
+                        :num="item.total"
+                        price="2.00"
+                        desc="描述信息"  
+                        title="商品标题"
+                        thumb="https://img.yzcdn.cn/vant/t-thirt.jpg">
+                        <div slot="bottom" style="margin-top:5px;">
+                            <van-row>
+                                <van-col span="8"> {{item.status}}</van-col>
+                                <van-col span="5"></van-col>
+                                <van-col span="11" style="text-align: right;">{{timestampToTime(item.orderTime)}}</van-col>
+                            </van-row>
+                            
+                        </div>
+                    </van-card>
+                </div>
+                <div v-else style="width:100%;">
+                    <img src="./images/tips2.jpg">
+                </div>
+                
             </van-tab>
-            <van-tab title="待付款" name="b">待付款</van-tab>
-            <van-tab title="待服务" name="c">待服务</van-tab>
-            <van-tab title="待评价" name="d">待评价</van-tab>
+            <van-tab title="待付款" name="b">
+                <div v-if="orderStatusFilter('待付款')!=''">
+                    <van-card
+                        v-for=" item in orderStatusFilter('待付款')"
+                        :key="item.id"
+                        :num="item.total"
+                        price="2.00"
+                        desc="描述信息"  
+                        title="商品标题"
+                        thumb="https://img.yzcdn.cn/vant/t-thirt.jpg">
+                        <div slot="bottom" style="margin-top:5px;">
+                            <van-row>
+                                <van-col span="8"> {{item.status}}</van-col>
+                                <van-col span="5"></van-col>
+                                <van-col span="11" style="text-align: right;">{{timestampToTime(item.orderTime)}}</van-col>
+                            </van-row>
+                            
+                        </div>
+                    </van-card>
+                </div>
+                <div v-else style="width:100%;">
+                    <img src="./images/tips2.jpg">
+                </div>
+                
+            </van-tab>
+            <van-tab title="待服务" name="c">
+                <div v-if="orderStatusFilter('未服务')!=''">
+                     <van-card
+                        v-for=" item in orderStatusFilter('未服务')"
+                        :key="item.id"
+                        :num="item.total"
+                        price="2.00"
+                        desc="描述信息"  
+                        title="商品标题"
+                        thumb="https://img.yzcdn.cn/vant/t-thirt.jpg">
+                        <div slot="bottom" style="margin-top:5px;">
+                            <van-row>
+                                <van-col span="8"> {{item.status}}</van-col>
+                                <van-col span="5"></van-col>
+                                <van-col span="11" style="text-align: right;">{{timestampToTime(item.orderTime)}}</van-col>
+                            </van-row>
+                            
+                        </div>
+                    </van-card>
+                </div>
+               <div v-else style="width:100%;">
+                    <img src="./images/tips2.jpg">
+                </div>
+               
+            </van-tab>
+            <van-tab title="待评价" name="d">
+                <div v-if="orderStatusFilter('待评价')!=''">
+                    <van-card
+                        v-for=" item in orderStatusFilter('待评价')"
+                        :key="item.id"
+                        :num="item.total"
+                        price="2.00"
+                        desc="描述信息"  
+                        title="商品标题"
+                        thumb="https://img.yzcdn.cn/vant/t-thirt.jpg">
+                        <div slot="bottom" style="margin-top:5px;">
+                            <van-row>
+                                <van-col span="8"> {{item.status}}</van-col>
+                                <van-col span="5"></van-col>
+                                <van-col span="11" style="text-align: right;">{{timestampToTime(item.orderTime)}}</van-col>
+                            </van-row>
+                            
+                        </div>
+                    </van-card>
+                </div>
+                <div v-else style="width:100%;">
+                    <img src="./images/tips2.jpg">
+                </div>
+                
+            </van-tab>
         </van-tabs>
+            <!-- {{orders}} -->
+
+        
+        
     </div>
 </template>
 <script>
+import {mapState,mapGetters,mapActions} from 'vuex'
 export default {
     name:'order',
     data() {
         return {
             activeName:'a',//记录当前选中标签页
         }
+    },
+    created(){
+        this.loadData();
+    },
+    computed:{
+        // 顾客的全部订单
+        ...mapState("order",["orders"]),
+        // 顾客id
+        ...mapState("customer",["customerId"]),
+        // 指定状态的订单
+        ...mapGetters("order",["orderStatusFilter"])
+    },
+    methods:{
+        // 查询顾客的所有订单的请求
+        ...mapActions("order",["loadCustomerOrderData"]),
+        // 普通方法
+        // fun:加载数据
+        loadData(){
+            this.loadCustomerOrderData(this.customerId);
+        },
+        // 时间转换
+        timestampToTime(datetime){
+            // let datetime = row.orderTime;
+                if(datetime){
+                    datetime = new Date(datetime);
+                    let y = datetime.getFullYear() + '-';
+                    let mon = datetime.getMonth()+1 + '-';
+                    let d = datetime.getDate() + ' ';
+                    let h = datetime.getHours() + ':'
+                    let m = datetime.getMinutes() + ':'
+                    let s = datetime.getSeconds()
+                    return y + mon + d + h + m + s;
+                }
+                return '';
+        }
+
+
     }
 }
 </script>
 <style scoped>
-
+.orderTab{
+    height: 50px;
+    
+}
 </style>
--- src/store/order.js
+++ src/store/order.js
@@ -5,12 +5,33 @@ export default{
         orders: [], // 存放顾客的所有的订单信息
     },
     getters:{
-        
+        // 需要为获取器传递参数的写法
+        orderStatusFilter(state){
+            // 对顾客的状态进行过滤
+            return function(status){
+                if(status){
+                    // 如果传递的状态存在则返回过滤
+                return state.orders.filter(item=>item.status===status);
+                }else{
+                    // 不存在返回全部
+                    return state.orders;
+                }
+            };
+        }
     },
     mutations:{
-
+        // 更新顾客订单信息
+        refreshCustomerOrder(state,orders){
+            state.orders = orders;
+        },
+        
     },
     actions:{
-        
+        // 获取指定顾客的所有订单信息
+        async loadCustomerOrderData({ commit },id) {
+            // 1.  传递分页查询所需的参数
+            const response = await get('/order/query', {customerId:id});
+            commit('refreshCustomerOrder', response.data);
+        } 
     }
 }
