0cf7ec04e155c78e25a604ddffcfe53f9a8500c3
llhlilihuan <794211268@qq.com>
2019-10-28 09:11:48 +0800

    [llh]App端product

--- src/pages/manager/Product.vue
+++ src/pages/manager/Product.vue
@@ -1,14 +1,36 @@
 <template>
-    <div id="order">
-       商品展示
+    <div id="product">
+       <van-nav-bar title="商品分类" />
+        <van-sidebar v-model="activeName">
+            <van-sidebar-item title="大衣" >
+                <van-card
+                    num="1"
+                    price="2.00"
+                    desc="描述信息"  
+                    title="商品标题"
+                    thumb="https://img.yzcdn.cn/vant/t-thirt.jpg">
+                    <van-stepper v-model="value"></van-stepper>
+                </van-card>
+            </van-sidebar-item>
+            <van-sidebar-item title="大衣外套" ></van-sidebar-item>
+            <van-sidebar-item title="裤装裙装" ></van-sidebar-item>
+            <van-sidebar-item title="配件" ></van-sidebar-item>
+        </van-sidebar>
+        <!-- <van-pagination 
+            v-model="currentPage" 
+            :total-items="125" 
+            :show-page-size="3" 
+            force-ellipses
+        /> -->
     </div>
 </template>
 <script>
 export default {
-    name:'order',
+    name:'product',
     data() {
       return {
-          
+            activeName:'a',//记录当前选中标签页
+            value: 1,
       }
     }
 }
--- src/store/product.js
+++ src/store/product.js
@@ -1,6 +1,8 @@
+import { get, post, post_array } from '@/http/axios'
 export default{
+    namespaced: true,
     state:{
-
+        products:[],//存放所有产品信息
     },
     getters:{
         
