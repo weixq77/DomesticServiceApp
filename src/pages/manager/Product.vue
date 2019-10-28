993209b1b1f232954943d8600158776340b30929
Pinkrayzys <857137603@qq.com>
2019-10-28 10:37:26 +0800

    [zys]产品的改善

--- src/pages/manager/Product.vue
+++ src/pages/manager/Product.vue
@@ -1,9 +1,24 @@
 <template>
     <div id="product">
        <van-nav-bar title="商品分类" />
+       <div id="left-nav">
         <van-sidebar v-model="activeName">
-            <van-sidebar-item title="大衣" >
-                <van-card
+            <van-sidebar-item title="大衣" name="a" >
+               
+            </van-sidebar-item>
+            <van-sidebar-item title="大衣外套" ></van-sidebar-item>
+            <van-sidebar-item title="裤装裙装" ></van-sidebar-item>
+            <van-sidebar-item title="配件" ></van-sidebar-item>
+        </van-sidebar>
+       </div>
+        <!-- <van-pagination 
+            v-model="currentPage" 
+            :total-items="125" 
+            :show-page-size="3" 
+            force-ellipses
+        /> -->
+        <div id="message">
+         <van-card
                     num="1"
                     price="2.00"
                     desc="描述信息"  
@@ -11,18 +26,61 @@
                     thumb="https://img.yzcdn.cn/vant/t-thirt.jpg">
                     <van-stepper v-model="value"></van-stepper>
                 </van-card>
-            </van-sidebar-item>
-            <van-sidebar-item title="大衣外套" ></van-sidebar-item>
-            <van-sidebar-item title="裤装裙装" ></van-sidebar-item>
-            <van-sidebar-item title="配件" ></van-sidebar-item>
-        </van-sidebar>
-        <!-- <van-pagination 
-            v-model="currentPage" 
-            :total-items="125" 
-            :show-page-size="3" 
-            force-ellipses
-        /> -->
+                 <van-card
+                    num="1"
+                    price="2.00"
+                    desc="描述信息"  
+                    title="商品标题"
+                    thumb="https://img.yzcdn.cn/vant/t-thirt.jpg">
+                    <van-stepper v-model="value"></van-stepper>
+                </van-card>
+                 <van-card
+                    num="1"
+                    price="2.00"
+                    desc="描述信息"  
+                    title="商品标题"
+                    thumb="https://img.yzcdn.cn/vant/t-thirt.jpg">
+                    <van-stepper v-model="value"></van-stepper>
+                </van-card>
+                 <van-card
+                    num="1"
+                    price="2.00"
+                    desc="描述信息"  
+                    title="商品标题"
+                    thumb="https://img.yzcdn.cn/vant/t-thirt.jpg">
+                    <van-stepper v-model="value"></van-stepper>
+                </van-card>
+                 <van-card
+                    num="1"
+                    price="2.00"
+                    desc="描述信息"  
+                    title="商品标题"
+                    thumb="https://img.yzcdn.cn/vant/t-thirt.jpg">
+                    <van-stepper v-model="value"></van-stepper>
+                </van-card>
+                 <van-card
+                    num="1"
+                    price="2.00"
+                    desc="描述信息"  
+                    title="商品标题"
+                    thumb="https://img.yzcdn.cn/vant/t-thirt.jpg">
+                    <van-stepper v-model="value"></van-stepper>
+                </van-card>
+                 <van-card
+                    num="1"
+                    price="2.00"
+                    desc="描述信息"  
+                    title="商品标题"
+                    thumb="https://img.yzcdn.cn/vant/t-thirt.jpg">
+                    <van-stepper v-model="value"></van-stepper>
+                </van-card>
+        </div>
         <van-submit-bar
+                :price="3050"
+                button-text="提交订单"
+                @submit="onSubmit"
+                />
+                 <van-submit-bar
                 :price="3050"
                 button-text="提交订单"
                 @submit="onSubmit"
@@ -46,5 +104,10 @@ export default {
 }
 </script>
 <style scoped>
-
+#message{
+    margin-left: 90px;
+}
+#left-nav{
+    position: absolute;
+}
 </style>
